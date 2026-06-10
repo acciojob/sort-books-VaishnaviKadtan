a6b1cc87835ac436398c33b38dc0efaf4bc595d2
VaishnaviKadtan <deepikakadtan15@gmail.com>
2026-06-10 13:08:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-
 import React from "react";
-import './../styles/App.css';
+import BooksList from "./components/BooksList";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <BooksList />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
